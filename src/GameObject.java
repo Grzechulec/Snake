db860921233177de736fb20430f2518a52fcfef3
grzegorzsmolak12 <grzegorzsmolak12@gmail.com>
2022-06-02 16:00:24 +0200

    GameObject changed

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,4 +1,14 @@
+import java.awt.Graphics;
 
 public abstract class GameObject {
 
+	protected int x, y;
+	
+	public GameObject(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public abstract void tick();
+	public abstract void render(Graphics g);
 }
