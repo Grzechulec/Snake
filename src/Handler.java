fc5293ca3f270f5af640f5b0dbaaa3444dd16645
grzegorzsmolak12 <grzegorzsmolak12@gmail.com>
2022-06-03 18:06:29 +0200

    Handler class added

---
+++ src/Handler.java
@@ -0,0 +1,27 @@
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+public class Handler {
+
+	ArrayList<GameObject> objects = new ArrayList<GameObject>();
+	
+	public void tick() {
+		for (GameObject object:objects) {
+            object.tick();
+        }
+	}
+	
+	public void render(Graphics g) {
+		for (GameObject object:objects) {
+            object.render(g);
+        }
+	}
+	
+	public void addObject(GameObject object) {
+		this.objects.add(object);
+	}
+	
+	public void removeObject(GameObject object) {
+		this.objects.remove(object);
+	}
+}
